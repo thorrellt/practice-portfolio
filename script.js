e3ea157bf813f6e45c72fb8a25dd1d05f76749fe
ThorrellT <thorrellt@gmail.com>
2022-02-05 00:59:32 -0500

    add JS logic fo effect to change Dev title

--- script.js
+++ script.js
@@ -34,3 +34,38 @@ for (let i = 0; i < links.length; i++) {
         })
     });
 }
+
+// TEXT EFFECT
+// TEXT ARRAY
+const dataText = ["Web Developer", "Ninja", "Retired Avenger", "Dad"];
+document.addEventListener("DOMContentLoaded", typingEffect);
+
+function typingEffect() {
+    function typeWriter(text, i, fnCallback) {
+        if (i < (text.length)) {
+            document.querySelector(".text-effect").innerHTML = text.substring(0, i + 1) + "<span class='effect'></span>";
+
+            // SET DELAY BEFORE NEXT CHARACTER
+            setTimeout(function () {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100)
+        } else if (typeof fnCallback == "function") {
+            setTimeout(fnCallback, 700);
+        }
+    }
+
+    // START Typing
+    function startTextAnimation(i) {
+        if (typeof dataText[i] == "undefined") {
+            setTimeout(function () {
+                startTextAnimation(0);
+            }, 2000)
+        }
+        if (i < dataText[i].length) {
+            typeWriter(dataText[i], 0, function () {
+                startTextAnimation(i + 1);
+            });
+        }
+    }
+    startTextAnimation(0);
+}
